9141be478e73bd981d82406541611c21540fa259
Patryk Smandzich <36845962+patryk-smandzich@users.noreply.github.com>
2024-03-09 15:49:54 +0100

    codegen device test

--- python_playwright/lesson03_codegen/codegen_demo.py
+++ python_playwright/lesson03_codegen/codegen_demo.py
@@ -1,7 +1,11 @@
 from playwright.sync_api import Page, expect
+import pytest
 
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args, playwright):
+    return {**playwright.devices["Pixel 7"]}
 
-def test_example(page: Page) -> None:
+def test_example_success_login(page: Page) -> None:
     page.goto("https://www.saucedemo.com/")
     page.locator("[data-test=\"username\"]").click()
     page.locator("[data-test=\"username\"]").fill("standard_user")
@@ -9,3 +13,12 @@ def test_example(page: Page) -> None:
     page.locator("[data-test=\"password\"]").fill("secret_sauce")
     page.locator("[data-test=\"login-button\"]").click()
     expect(page.get_by_text("Swag Labs")).to_be_visible()
+
+def test_example_fail_login(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").click()
+    page.locator("[data-test=\"username\"]").fill("standard_user")
+    page.locator("[data-test=\"password\"]").click()
+    page.locator("[data-test=\"password\"]").fill("wrong pass")
+    page.locator("[data-test=\"login-button\"]").click()
+    expect(page.locator("[data-test=\"error\"]")).to_contain_text("Epic sadface: Username and password do not match any user in this service")
